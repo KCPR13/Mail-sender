0ac82a592f9e146e8885911482c8f951fcef3cdd
Kacper <kacper.misterski13@gmail.com>
2018-05-01 19:23:31 +0200

    comments were added and code was cleaned

--- mailSender/Form1.Designer.cs
+++ mailSender/Form1.Designer.cs
@@ -29,18 +29,18 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(mailSender));
-            this.Email = new System.Windows.Forms.Label();
+            this.usernameLabel = new System.Windows.Forms.Label();
             this.usernameTextBox = new System.Windows.Forms.TextBox();
             this.PasswordTextBox = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
+            this.passwordLabel = new System.Windows.Forms.Label();
             this.buttonSend = new System.Windows.Forms.Button();
             this.portDomain = new System.Windows.Forms.DomainUpDown();
-            this.label2 = new System.Windows.Forms.Label();
+            this.portLabel = new System.Windows.Forms.Label();
             this.smtpLabel = new System.Windows.Forms.Label();
             this.sslCheckBox = new System.Windows.Forms.CheckBox();
             this.smtpCheckedListBox = new System.Windows.Forms.CheckedListBox();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
+            this.toLabel = new System.Windows.Forms.Label();
+            this.subjectLabel = new System.Windows.Forms.Label();
             this.toTextBox = new System.Windows.Forms.TextBox();
             this.subjectTextBox = new System.Windows.Forms.TextBox();
             this.messageTextBox = new System.Windows.Forms.TextBox();
@@ -49,23 +49,23 @@
             this.procentageLabel = new System.Windows.Forms.Label();
             this.attachementListBox = new System.Windows.Forms.ListBox();
             this.delAtchButton = new System.Windows.Forms.Button();
-            this.label5 = new System.Windows.Forms.Label();
+            this.emailLabel = new System.Windows.Forms.Label();
             this.emailTextBox = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.attachementPictureBox)).BeginInit();
             this.SuspendLayout();
             // 
-            // Email
+            // usernameLabel
             // 
-            this.Email.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            this.usernameLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.Email.AutoSize = true;
-            this.Email.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.Email.Location = new System.Drawing.Point(5, 20);
-            this.Email.Name = "Email";
-            this.Email.Size = new System.Drawing.Size(91, 20);
-            this.Email.TabIndex = 0;
-            this.Email.Text = "Username:";
+            this.usernameLabel.AutoSize = true;
+            this.usernameLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.usernameLabel.Location = new System.Drawing.Point(5, 20);
+            this.usernameLabel.Name = "usernameLabel";
+            this.usernameLabel.Size = new System.Drawing.Size(91, 20);
+            this.usernameLabel.TabIndex = 0;
+            this.usernameLabel.Text = "Username:";
             // 
             // usernameTextBox
             // 
@@ -88,15 +88,15 @@
             this.PasswordTextBox.TabIndex = 4;
             this.PasswordTextBox.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
             // 
-            // label1
+            // passwordLabel
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label1.Location = new System.Drawing.Point(5, 60);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(88, 20);
-            this.label1.TabIndex = 5;
-            this.label1.Text = "Password:";
+            this.passwordLabel.AutoSize = true;
+            this.passwordLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.passwordLabel.Location = new System.Drawing.Point(5, 60);
+            this.passwordLabel.Name = "passwordLabel";
+            this.passwordLabel.Size = new System.Drawing.Size(88, 20);
+            this.passwordLabel.TabIndex = 5;
+            this.passwordLabel.Text = "Password:";
             // 
             // buttonSend
             // 
@@ -124,22 +124,20 @@
             this.portDomain.Size = new System.Drawing.Size(110, 22);
             this.portDomain.TabIndex = 9;
             this.portDomain.Text = "587";
-            this.portDomain.SelectedItemChanged += new System.EventHandler(this.portDomain_SelectedItemChanged);
             // 
-            // label2
+            // portLabel
             // 
-            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            this.portLabel.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label2.Location = new System.Drawing.Point(8, 560);
-            this.label2.MaximumSize = new System.Drawing.Size(60, 20);
-            this.label2.MinimumSize = new System.Drawing.Size(60, 20);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(60, 20);
-            this.label2.TabIndex = 10;
-            this.label2.Text = "Port:";
-            this.label2.Click += new System.EventHandler(this.label2_Click);
+            this.portLabel.AutoSize = true;
+            this.portLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.portLabel.Location = new System.Drawing.Point(8, 560);
+            this.portLabel.MaximumSize = new System.Drawing.Size(60, 20);
+            this.portLabel.MinimumSize = new System.Drawing.Size(60, 20);
+            this.portLabel.Name = "portLabel";
+            this.portLabel.Size = new System.Drawing.Size(60, 20);
+            this.portLabel.TabIndex = 10;
+            this.portLabel.Text = "Port:";
             // 
             // smtpLabel
             // 
@@ -186,27 +184,26 @@
             this.smtpCheckedListBox.Name = "smtpCheckedListBox";
             this.smtpCheckedListBox.Size = new System.Drawing.Size(396, 89);
             this.smtpCheckedListBox.TabIndex = 14;
-            this.smtpCheckedListBox.SelectedIndexChanged += new System.EventHandler(this.smtpCheckedListBox_SelectedIndexChanged);
             // 
-            // label3
+            // toLabel
             // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label3.Location = new System.Drawing.Point(5, 140);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(33, 20);
-            this.label3.TabIndex = 15;
-            this.label3.Text = "To:";
+            this.toLabel.AutoSize = true;
+            this.toLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.toLabel.Location = new System.Drawing.Point(5, 140);
+            this.toLabel.Name = "toLabel";
+            this.toLabel.Size = new System.Drawing.Size(33, 20);
+            this.toLabel.TabIndex = 15;
+            this.toLabel.Text = "To:";
             // 
-            // label4
+            // subjectLabel
             // 
-            this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label4.Location = new System.Drawing.Point(5, 180);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(70, 20);
-            this.label4.TabIndex = 16;
-            this.label4.Text = "Subject:";
+            this.subjectLabel.AutoSize = true;
+            this.subjectLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.subjectLabel.Location = new System.Drawing.Point(5, 180);
+            this.subjectLabel.Name = "subjectLabel";
+            this.subjectLabel.Size = new System.Drawing.Size(70, 20);
+            this.subjectLabel.TabIndex = 16;
+            this.subjectLabel.Text = "Subject:";
             // 
             // toTextBox
             // 
@@ -286,7 +283,6 @@
             this.attachementListBox.Name = "attachementListBox";
             this.attachementListBox.Size = new System.Drawing.Size(648, 84);
             this.attachementListBox.TabIndex = 23;
-            this.attachementListBox.SelectedIndexChanged += new System.EventHandler(this.attachementListBox_SelectedIndexChanged);
             // 
             // delAtchButton
             // 
@@ -303,15 +299,15 @@
             this.delAtchButton.UseVisualStyleBackColor = true;
             this.delAtchButton.Click += new System.EventHandler(this.delAtchButton_Click);
             // 
-            // label5
+            // emailLabel
             // 
-            this.label5.AutoSize = true;
-            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label5.Location = new System.Drawing.Point(5, 100);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(62, 20);
-            this.label5.TabIndex = 28;
-            this.label5.Text = "E-mail:";
+            this.emailLabel.AutoSize = true;
+            this.emailLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.emailLabel.Location = new System.Drawing.Point(5, 100);
+            this.emailLabel.Name = "emailLabel";
+            this.emailLabel.Size = new System.Drawing.Size(62, 20);
+            this.emailLabel.TabIndex = 28;
+            this.emailLabel.Text = "E-mail:";
             // 
             // emailTextBox
             // 
@@ -328,7 +324,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(682, 653);
-            this.Controls.Add(this.label5);
+            this.Controls.Add(this.emailLabel);
             this.Controls.Add(this.emailTextBox);
             this.Controls.Add(this.delAtchButton);
             this.Controls.Add(this.attachementListBox);
@@ -338,18 +334,18 @@
             this.Controls.Add(this.messageTextBox);
             this.Controls.Add(this.subjectTextBox);
             this.Controls.Add(this.toTextBox);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.label3);
+            this.Controls.Add(this.subjectLabel);
+            this.Controls.Add(this.toLabel);
             this.Controls.Add(this.smtpCheckedListBox);
             this.Controls.Add(this.sslCheckBox);
             this.Controls.Add(this.smtpLabel);
-            this.Controls.Add(this.label2);
+            this.Controls.Add(this.portLabel);
             this.Controls.Add(this.portDomain);
             this.Controls.Add(this.buttonSend);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.passwordLabel);
             this.Controls.Add(this.PasswordTextBox);
             this.Controls.Add(this.usernameTextBox);
-            this.Controls.Add(this.Email);
+            this.Controls.Add(this.usernameLabel);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.MinimumSize = new System.Drawing.Size(700, 700);
@@ -366,18 +362,18 @@
 
         #endregion
 
-        private System.Windows.Forms.Label Email;
+        private System.Windows.Forms.Label usernameLabel;
         private System.Windows.Forms.TextBox usernameTextBox;
         private System.Windows.Forms.TextBox PasswordTextBox;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label passwordLabel;
         private System.Windows.Forms.Button buttonSend;
         private System.Windows.Forms.DomainUpDown portDomain;
-        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label portLabel;
         private System.Windows.Forms.Label smtpLabel;
         private System.Windows.Forms.CheckBox sslCheckBox;
         private System.Windows.Forms.CheckedListBox smtpCheckedListBox;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label toLabel;
+        private System.Windows.Forms.Label subjectLabel;
         private System.Windows.Forms.TextBox toTextBox;
         private System.Windows.Forms.TextBox subjectTextBox;
         private System.Windows.Forms.TextBox messageTextBox;
@@ -386,7 +382,7 @@
         private System.Windows.Forms.Label procentageLabel;
         private System.Windows.Forms.ListBox attachementListBox;
         private System.Windows.Forms.Button delAtchButton;
-        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label emailLabel;
         private System.Windows.Forms.TextBox emailTextBox;
     }
 }
--- mailSender/Form1.cs
+++ mailSender/Form1.cs
@@ -14,188 +14,141 @@ using System.Net.Mime;
 
 namespace mailSender
 {
-    
-    
+
     public partial class mailSender : Form
     {
-        
+
         OpenFileDialog ofd = new OpenFileDialog();
         MailMessage msg = new MailMessage();
         List<int> sizeAttachement = new List<int>();
         List<string> nameAttachement = new List<string>();
-        long size;
-        int procentage;
-        string path;
-     
+        long sizeSelectedAttachment;
+        int procentageProgressBar;
+        string AttachmentPath;
+
         public mailSender()
         {
-            InitializeComponent();       
+            InitializeComponent();
         }
-        
+
         private void Form1_Load(object sender, EventArgs e)
         {
-            DomainUpDown.DomainUpDownItemCollection collection = this.portDomain.Items;   
-            collection.Add("587");
+            DomainUpDown.DomainUpDownItemCollection collection = this.portDomain.Items;
+            collection.Add("587");  //2 typy portow 
             collection.Add("25");
-            portDomain.SelectedIndex = 0;
+            portDomain.SelectedIndex = 0; //domyslny indeks 0
             attachementListBox.Hide();
-        }
-
-        private void label1_Click(object sender, EventArgs e)
-        {
-
         }
 
         private void textBox2_TextChanged(object sender, EventArgs e)
         {
-            PasswordTextBox.PasswordChar = '*'; 
-            PasswordTextBox.MaxLength = 32;         
-        }
-
-        private void Save_CheckedChanged(object sender, EventArgs e)
-        {
-            
+            PasswordTextBox.PasswordChar = '*'; //szyfrowanie hasła znakiem
+            PasswordTextBox.MaxLength = 32;
         }
 
         private void buttonLogin_Click(object sender, EventArgs e)
         {
-            
-            int counter;
-            counter = smtpCheckedListBox.CheckedItems.Count;
-            if (counter > 1) MessageBox.Show(string.Format("Select only one SMTP adress!"), "Warning!", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            else if (counter == 0) MessageBox.Show(string.Format("Select one SMTP adress!"), "Warning!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            int CheckedSMTPAdressCounter;
+            CheckedSMTPAdressCounter = smtpCheckedListBox.CheckedItems.Count;
+            if (CheckedSMTPAdressCounter > 1) MessageBox.Show(string.Format("Select only one SMTP adress!"), "Warning!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            else if (CheckedSMTPAdressCounter == 0) MessageBox.Show(string.Format("Select one SMTP adress!"), "Warning!", MessageBoxButtons.OK, MessageBoxIcon.Error);
             else
             {
-             
-                    using (SmtpClient client = new SmtpClient())
-                    {
+                using (SmtpClient client = new SmtpClient())
+                {
+                    bool mailSent; //flaga ktora informuje czy mail zostal wyslany
                     client.EnableSsl = sslCheckBox.Checked;
-                    try
-                       {
-                             var credential = new NetworkCredential
-                             {
-                                  UserName = usernameTextBox.Text,
-                                  Password = PasswordTextBox.Text
-                             };
-                             client.Credentials = credential;
-                             for (int z = 0; z < smtpCheckedListBox.Items.Count; z++)
-                             {
-                                 if (smtpCheckedListBox.GetItemChecked(z))
-                                 {
-                                string str = (string)smtpCheckedListBox.Items[z];
-                                client.Host = str;
-                                 }
-                             }
-                             client.Port = Int32.Parse(portDomain.SelectedItem.ToString());
-                             
-                             msg.To.Add(new MailAddress(toTextBox.Text));
-                             msg.From = new MailAddress(emailTextBox.Text);
-                             msg.Subject = subjectTextBox.Text;
-                             msg.Body = messageTextBox.Text;
-                             msg.IsBodyHtml = true;
-                             client.Send(msg);
-                       }
-                       catch
-                       {
-                            MessageBox.Show(string.Format("Email cannot be sent! "), "Warning!", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                       }
+                    if (!sslCheckBox.Checked)
+                    {
+                        MessageBox.Show(string.Format("Please check SSL box"), "Information", MessageBoxButtons.OK, MessageBoxIcon.Warning);
                     }
+                    try
+                    {
+                        var credential = new NetworkCredential
+                        {
+                            UserName = usernameTextBox.Text,
+                            Password = PasswordTextBox.Text
+                        };
+                        client.Credentials = credential;
+                        for (int z = 0; z < smtpCheckedListBox.Items.Count; z++)
+                        {
+                            if (smtpCheckedListBox.GetItemChecked(z))
+                            {
+                                string str = (string)smtpCheckedListBox.Items[z];
+                                client.Host = str;  // przypisanie hosta emaila
+                            }
+                        }
+                        client.Port = Int32.Parse(portDomain.SelectedItem.ToString()); //konwersja do inta portu
+                        msg.To.Add(new MailAddress(toTextBox.Text));
+                        msg.From = new MailAddress(emailTextBox.Text);
+                        msg.Subject = subjectTextBox.Text;
+                        msg.Body = messageTextBox.Text;
+                        msg.IsBodyHtml = true; // ciało wiadomosci jest w HTML
+                        client.Send(msg);
+                        mailSent = true;
+                    }
+                    catch
+                    {
+                        if (sslCheckBox.Checked)
+                        {
+                            MessageBox.Show(string.Format("Email cannot be sent! "), "Warning!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        }
+                        mailSent = false;
+                    }
+                    if (mailSent)
+                    {
+                        MessageBox.Show(string.Format("E-mail has been succesfully sent"), "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                }
             }
         }
-
-        private void label2_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void textBox3_TextChanged(object sender, EventArgs e)
-        {
-          
-        }
-
-        private void smtpCheckedListBox_SelectedIndexChanged(object sender, EventArgs e)
-        {
-
-        }
-        private void smtpCheckedListBox_ItemCheck(object sender, ItemCheckEventArgs e)
-        {
-            
-        }
-
-        private void portDomain_SelectedItemChanged(object sender, EventArgs e)
-        {
-
-        }
-
         private void attachementPictureBox_Click(object sender, EventArgs e)
-        { 
+        {
             ofd.Filter = "All files (*.*)|*.*"; //filtr danych w ofd
-            if(ofd.ShowDialog()==DialogResult.OK) //wykonuje sie tylko gdy nacisniesz ok
+            if (ofd.ShowDialog() == DialogResult.OK) //wykonuje sie tylko gdy nacisniesz ok
             {
-                path = ofd.FileName.ToString();
+                AttachmentPath = ofd.FileName.ToString();
                 FileInfo info = new FileInfo(ofd.FileName);
                 sizeAttachement.Add(Convert.ToInt32(info.Length / (1024 * 1024)));
                 nameAttachement.Add(ofd.FileName);
-                size = info.Length/(1024*1024); //zamiana na mb
+                sizeSelectedAttachment = info.Length / (1024 * 1024); //zamiana na mb
                 attachementProgressBar.Minimum = 0;
                 attachementProgressBar.Maximum = 25;
-               
-                if (size > 25)
+
+                if (sizeSelectedAttachment > 25)
                 {
                     MessageBox.Show(string.Format("Attachment must be smaller than 25mb!"), "Warning!", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                }else
+                }
+                else
                 {
-                    msg.Attachments.Add(new Attachment(path));
-                    attachementProgressBar.Increment(Convert.ToInt32(size));
-                    procentage = attachementProgressBar.Value * 4;
-                    procentageLabel.Text = Convert.ToString(procentage) + "%";
+                    msg.Attachments.Add(new Attachment(AttachmentPath));
+                    attachementProgressBar.Increment(Convert.ToInt32(sizeSelectedAttachment));
+                    procentageProgressBar = attachementProgressBar.Value * 4; // mnozenie aby zakres wynosil 0-100%
+                    procentageLabel.Text = Convert.ToString(procentageProgressBar) + "%";
                     attachementListBox.Items.Add(info.Name);
                     attachementListBox.Show();
-                }             
+                }
             }
         }
-
-        private void attachementListBox_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            
-        }
-
         private void delAtchButton_Click(object sender, EventArgs e)
         {
-            if (attachementListBox.SelectedIndex == -1)
-            {
-
-            }
-            else
+            if (attachementListBox.SelectedIndex != -1)
             {
                 ListBox.SelectedObjectCollection selectedItems = new ListBox.SelectedObjectCollection(attachementListBox);
                 selectedItems = attachementListBox.SelectedItems;
-                if (attachementListBox.SelectedIndex != -1)
+
+                int attachementListBoxIndex = attachementListBox.SelectedIndex;
+                for (int i = selectedItems.Count - 1; i >= 0; i--)
                 {
-                    int attachementListBoxindex = attachementListBox.SelectedIndex;
-                    for (int i = selectedItems.Count - 1; i >= 0; i--)
-                    attachementListBox.Items.Remove(selectedItems[i]);  
-                    msg.Attachments.RemoveAt(attachementListBoxindex);
-                    attachementProgressBar.Increment(-sizeAttachement[attachementListBoxindex]);
-                    sizeAttachement.RemoveAt(attachementListBoxindex);
-                    procentage = attachementProgressBar.Value * 4;
-                    procentageLabel.Text = Convert.ToString(procentage) + "%";
-       
-                    for(int z= nameAttachement.Count-1; z>=0; z--)
-                    {
-                        foreach (Attachment attachment in msg.Attachments)
-                        {
-                            if (attachment.Name == Convert.ToString(nameAttachement[z]))
-                            {
-                                msg.Attachments.Remove(attachment);
-                                break;
-                            }
-                        }
-                    }
+                    attachementListBox.Items.Remove(selectedItems[i]);
                 }
                 
-
-
+                msg.Attachments.RemoveAt(attachementListBoxIndex); //usuwanie wybranego załącznika
+                attachementProgressBar.Increment(-sizeAttachement[attachementListBoxIndex]); //dekrementacja progressbara
+                sizeAttachement.RemoveAt(attachementListBoxIndex); // usuwa wage pliku z listy
+                procentageProgressBar = attachementProgressBar.Value * 4; // uaktualnij wartosc progressbara
+                procentageLabel.Text = Convert.ToString(procentageProgressBar) + "%";         
             }
         }
     }
